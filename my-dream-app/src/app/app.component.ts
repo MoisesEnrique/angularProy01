4cc39337c58e7c6ec44d3c333e16e3d960a17fb2
MoisesEnrique <moises.mac@gmail.com>
2021-07-26 23:43:17 -0500

    Angular 1 - Creando el costructor y el metodo show hobbies

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -7,7 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
-  name = 'Moises Enrique';    //definimos la variables
-  email = 'moises@unsa.edu.pe';
-  webpage = 'http://www.unsa.edu.pe';
+  name: string;   //definimos el tipo de la variable
+  email;
+  webpage: string;
+  hobbies: string[];  //arreglo de strings
+
+
+  constructor() {   //constructor que se instancia al inicio
+    this.name = 'Moises Enrique';    //definimos la variables
+    this.email = 'moises@unsa.edu.pe';
+    this.webpage = 'http://www.unsa.edu.pe';
+    this.hobbies = ["Futbol", "Programacion", "Netflix"];   //para utilizar el for
+  }
+
+  showhobbies() {   //creamos el metodo para mostrar los hobbies con el uso de un if
+    return true;
+  }
 }
