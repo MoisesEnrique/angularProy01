b997dc9125f6c08aded4c5d916e4e947d9a43dd1
MoisesEnrique <moises.mac@gmail.com>
2021-07-26 23:25:29 -0500

    Angular 1 - Definiendo variables en component.ts

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({    //para trabajar componentes
+  selector: 'app-root',  // para seleccionar la eiqueta en el index.html
+  templateUrl: './app.component.html',  //para llamar el component html
+  styleUrls: ['./app.component.css']    //para llamar el component css
 })
 export class AppComponent {
   title = 'my-dream-app';
+  name = 'Moises Enrique';    //definimos la variables
+  email = 'moises@unsa.edu.pe';
+  webpage = 'http://www.unsa.edu.pe';
 }
