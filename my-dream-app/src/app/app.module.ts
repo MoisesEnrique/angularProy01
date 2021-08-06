0e83a7d92517d58235a6c0974e2d66f4751723c0
MoisesEnrique <moises.mac@gmail.com>
2021-08-06 02:55:46 -0500

    Angular 2 - Creando component about y ruteos

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -10,19 +10,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
 
+import { RouterModule, Route } from '@angular/router';
+
+const routes: Route[] = [
+  {path: '', component: AppComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'hello', component: HelloWorldComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
